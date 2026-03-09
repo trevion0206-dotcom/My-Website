a5ba02586366274c7903d81cea207fe2d7a652c2
trevion0206-dotcom <trevion0206@icloud.com>
2026-03-08 22:34:50 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
  function videoFunction(){
             document.getElementById("myComment").innerHTML += "A great vaction video";
 }
+
+function emailMessage(){
+    document.getElementById("emailBox").innerHTML += "Please enter valid email address";
+}
+
+function blankMessage(){
+    document.getElementById("emailBox").innerHTML = "Email:";
+}
